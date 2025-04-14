f02fd5385ec5838e92c39d60ea1fe309a12b4825
sdekhterman <sdekhterman@gmail.com>
2025-04-14 12:46:34 -0500

    messed with pointers
    More reference and assignment testing.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
-
+using namespace std;
 int main(int argc, char *argv[], char *envp[])
 {
-    int value = 42;
-    int* ptr = &value;
+    // int value = 42;
+    // int* ptr = &value;
 
-    std::cout << "Value: " << *ptr << std::endl;
+    // std::cout << "Value: " << *ptr << std::endl;
+
+    // int a = 12;
+    // int b = 90;
+
+    // int *ptr = &a;
+    // std::cout << *ptr << std::endl;
+    
+    // // Changing the address stored
+    // ptr = &b;
+    // std::cout << *ptr;
+
+    // int *ptr1;
+    // char *ptr2;
+    
+    // // Finding size using sizeof()
+    // cout << sizeof(ptr1) << endl;
+    // cout << sizeof(ptr2);
+
+    // while(true){
+    //     int* p = (int*)malloc(sizeof(int));
+    //     cout << "Print an assigned pointer at memory allocation location: " << *p << endl;
+    //     int *ptr;
+    //     cout << "Crash program by trying to print an unassigned pointer: " << *ptr << endl;
+    // }
+
+    // NULL pointer
+    int *ptr = NULL;
 
     return 0;
 }
